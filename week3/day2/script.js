211743453a1d8cd2121efc0bd2d9263329a0bb1b
TeilahBaron <139397326+TeilahBaron@users.noreply.github.com>
2023-12-11 17:52:31 +0200

    commit

---
+++ week3/day2/script.js
@@ -0,0 +1,11 @@
+// Exercise 1: Your Favorite Food
+
+// let faviriteFood = 'pizza'
+// let favoriteMeal = 'dinner'
+// console.log( 'I eat'+ ' '+ faviriteFood+ ' '+ 'at every'+ ' '+favoriteMeal)
+
+// Exercise 2 : Series
+
+// const myWatchedSeries = ["black mirror", "money heist", "the big bang theory"]
+// let myWatchedSeriesLength =' '+ 4 + ' ' +'series'+ ' ' + myWatchedSeries
+// console.log( 'I watched'+ myWatchedSeriesLength)
