095daa72ba4598fcc5608115124ec1b9c8cfcb55
TeilahBaron <139397326+TeilahBaron@users.noreply.github.com>
2023-12-11 16:37:43 +0200

    1

---
+++ week3/day1/scipt.js
@@ -0,0 +1,82 @@
+//console.log
+// console.log("Hello world")
+// console.log("this is day 1 of js")
+
+//Veriebles
+// let x=3
+// console.log(x)
+
+// keywords
+// let x, y
+
+// x = 5 + 6
+// console.log(x)
+
+// y = x * 10
+
+// console.log(y)
+
+// DataTypes
+// Strings
+// console.log('Hello')
+// let a= "Hello"
+// let b="World"
+// let c = a + " " + b
+// console.log(c)
+// console.log(
+//     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, aliquid explicabo quisquam fugit officiis beatae obcaecati. Maxime saepe aperiam corrupti!Lorem ipsum dolor  sit amet, consectetur adipisicing elit. Iste, aliquid explicabo quisquam fugit officiis beatae obcaecati. Maxime saepe aperiam corrupti!
+// )
+
+// String Properties
+// let longString="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, aliquid explicabo quisquam fugit officiis beatae obcaecati. Maxime saepe aperiam corrupti!Lorem ipsum dolor  sit amet, consectetur adipisicing elit. Iste, aliquid explicabo quisquam fugit officiis beatae obcaecati. Maxime saepe aperiam corrupti!";
+
+// let longStringLength = longString.length;
+// console.log(longString.length)
+
+// String Methods
+// let str = "Hello Everyone, pleas say hello to the class";
+// let posUppercaseHello = str.indexOf("Hello");
+// let posLowercaseHello = str.indexOf("hello");
+// console.log("Hello Everyone, pleas say hello to the class");
+// console.log(posUppercaseHello);
+// console.log(posLowercaseHello);
+// let addressNumber = 19;
+// console.log(addressNumber)
+// let addressStreet = "Ben Yehuda"
+// console.log(addressStreet)
+// let country = "Israel"
+// console.log(country)
+// Exercise 2
+// let birthYear =1988
+// let futureYear = 2025
+// let age = futureYear - birthYear
+// console.log("I will be"+' ' + age +' ' + "in" +' ' + futureYear)
+
+// arrays
+// let user1 ="John"
+// let user2 ="Bill"
+// let user3 ="Jon"
+
+// let users=['John', 'Bill', 'Nancy']
+// console.log(users[2])
+
+// let nestedArrays = [
+//     [1, 2, 3],
+//     [4, 'hi', 6],
+//     [7, 8, 9],
+// ];
+// console.log(nestedArrays)
+// console.log(nestedArrays[1][1])
+// changing element
+// let colors= ['blue', 'yellow', 'green'];
+// console.log(colors)
+// colors[1] = 'pink';
+// console.log(colors)
+
+// array methods:
+
+let colors=[ "blue", "yellow", "green"];
+// colors.splice(1, 1, "pink", "white", "black")
+// console.log(colors)
+colors.splice(1,1,"pink", "grey")
+console.log(colors)
