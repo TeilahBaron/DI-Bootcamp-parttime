34a99b5bcbfb2a80b2a11f6c3b35606c04c281bc
TeilahBaron <139397326+TeilahBaron@users.noreply.github.com>
2023-12-12 21:02:40 +0200

    day3

--- week3/script.js
+++ week3/script.js
@@ -1,8 +1,18 @@
-let user = prompt( 'What is your age?');
+// let user = prompt( 'What is your age?');
 
-    if (user< 18){
-        alert('Sorry');
-    }
-    else if( user>18){
-        alert('Congratulations')
-    }
+//     if (user< 18){
+//         alert('Sorry');
+//     }
+//     else if( user>18){
+//         alert('Congratulations')
+//     }
+let user={
+    username:'Teilah',
+    password:'1234',
+};
+
+let database =[user]
+let newsfeed = [{ username:'Teilah' , timeline:'2005-2009'},
+ { username:'Bar' , timeline:'2005-2009'},
+]
+console.log(newsfeed)
