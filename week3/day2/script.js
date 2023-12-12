1251e718ec81115ff969922121c6e916a1c60422
TeilahBaron <139397326+TeilahBaron@users.noreply.github.com>
2023-12-12 17:58:09 +0200

    commit

--- week3/day2/script.js
+++ week3/day2/script.js
@@ -4,8 +4,66 @@
 // let favoriteMeal = 'dinner'
 // console.log( 'I eat'+ ' '+ faviriteFood+ ' '+ 'at every'+ ' '+favoriteMeal)
 
-// Exercise 2 : Series
+// Exercise 2 : Series part1
 
-// const myWatchedSeries = ["black mirror", "money heist", "the big bang theory"]
+// let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"]
 // let myWatchedSeriesLength =' '+ 4 + ' ' +'series'+ ' ' + myWatchedSeries
 // console.log( 'I watched'+ myWatchedSeriesLength)
+
+// Part2.1
+
+// let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"]
+// let myWatchedSeriesLength =' '+ 4 + ' ' +'series'+ ' ' + myWatchedSeries
+// console.log( 'I watched'+ myWatchedSeriesLength)
+// // myWatchedSeries.pop('the big bang theory')
+// // console.log( myWatchedSeries)
+// // myWatchedSeries.push('friends')
+// // console.log( myWatchedSeries)
+
+
+//part2.2
+
+// let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"]
+// console.log(myWatchedSeries)
+// myWatchedSeries[2]='friends'
+// console.log(myWatchedSeries)
+// myWatchedSeries.push("Crown")
+// console.log(myWatchedSeries)
+// myWatchedSeries.shift()
+// console.log(myWatchedSeries)
+
+
+// 🌟 Exercise 3 : The Temperature Converter
+// Store a celsius temperature into a variable.
+
+// Convert it to fahrenheit and console.log <temperature>°C is <temperature>°F.
+// Hint : Should you create another variable to hold the temperature in fahrenheit? (ie. point 2)
+// Hint: To convert a temperature from celsius to fahrenheit : Divide it by 5, then multiply it by 9, then add 32
+
+// let celsius = 1
+// let cTemp = celsius;
+// let cToFar = cTemp*9/5 + 32;
+// let result = cTemp +'°C is'+ ' ' +cToFar+ '°F'
+// console.log(result)
+
+
+
+
+// Exercise 4 : Guess The Answers #1
+
+    // let c;
+    // let a = 34;
+    // let b = 21;
+    //Prediction: It will output 55, because 34 and 21 are numbers
+    // actual 55 value of c=0
+
+    // console.log(a+b) 
+
+    // a=2
+
+    // console.log(a+b) //second expression
+    // Prediction: it will be 23 because a=2, number changed
+    // Actual:23
+
+    
+    // console.log(typeof 'potato')
