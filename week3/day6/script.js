df75121ca97d78b3bceb572269213df2000c475c
TeilahBaron <139397326+TeilahBaron@users.noreply.github.com>
2023-12-17 17:31:40 +0200

    day6

---
+++ week3/day6/script.js
@@ -0,0 +1,51 @@
+// Exercise 1 : List Of People
+// Write code to remove “Greg” from the people array.
+const people = ["Greg", "Mary", "Devon", "James"];
+// people.splice(0,1)
+// // console.log(people)
+// people.splice(2,1,"Jason")
+// people.push('Teilah')
+// console.log(people)
+// console.log(people.indexOf())
+// var copiedPeople = people.slice(1, people.length)
+// console.log(copiedPeople)
+// copiedPeople.push("Foo")
+// console.log(copiedPeople.indexOf())
+
+// let last = copiedPeople[2]
+// console.log(last)
+
+// Part2
+// Using a loop, iterate through the people array and console.log each person.
+
+// Using a loop, iterate through the people array and exit the loop after you console.log “Devon” .
+// Hint: take a look at the break statement in the lesson.
+// for( let i = 0; i< people.length; i++){
+//     console.log(people,i);
+    
+//     }
+
+
+// 🌟 Exercise 2 : Your Favorite Colors
+
+// let colors =["blue", "green", "red", "purple","black"] 
+// for(let i= 0 ; i < colors.length; i++){
+//     console.log("My"  +"is"+colors[i])
+// }
+
+// Exercise 3 : Repeat The Question
+
+// Prompt the user for a number.
+// Hint : Check the data type you receive from the prompt (ie. Use the typeof method)
+
+// While the number is smaller than 10 continue asking the user for a new number.
+// Tip : Which while loop is more relevant for this situation?
+
+// Exercise 4 : Building Management
+
+// 🌟 Exercise 5 : Family
+
+// Exercise 6 : Rudolf
+// Exercise 7 : Secret Group
+
+// dont have a time:(
