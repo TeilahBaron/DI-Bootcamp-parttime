9233398cb35fc08515a3ad8166fa489f01749509
TeilahBaron <139397326+TeilahBaron@users.noreply.github.com>
2024-02-24 18:11:08 +0200

    w7d4challenge

---
+++ week7/day4/w7d4challeng.sql
@@ -0,0 +1,12 @@
+SELECT * FROM Library;
+
+SELECT s.name, b.title
+FROM Library l
+JOIN Student s ON l.student_fk_id = s.student_id
+JOIN Book b ON l.book_fk_id = b.book_id;
+
+SELECT AVG(s.age) AS average_age
+FROM Library l
+JOIN Student s ON l.student_fk_id = s.student_id
+JOIN Book b ON l.book_fk_id = b.book_id
+WHERE b.title = 'Alice In Wonderland';
