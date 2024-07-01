7a43456f2e651ded5b08ed435dea3942474102d9
TeilahBaron <139397326+TeilahBaron@users.noreply.github.com>
2024-07-01 19:24:41 +0300

    ex4

---
+++ week14/day2/ex4/my-react-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Color from './Color'; // Import the Color component
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>My React App</h1>
+      <Color /> {/* Render the Color component here */}
+    </div>
+  );
+};
+
+export default App;
---
+++ week14/day2/ex4/my-react-app/src/Color.js
@@ -0,0 +1,24 @@
+// Color.js
+
+import React, { useState, useEffect } from 'react';
+
+const Color = () => {
+  const [favoriteColor, setFavoriteColor] = useState("red");
+
+  useEffect(() => {
+    alert("useEffect reached");
+  }, []);
+
+  const changeColor = () => {
+    setFavoriteColor("blue");
+  };
+
+  return (
+    <div>
+      <h1>{favoriteColor}</h1>
+      <button onClick={changeColor}>Change Color to Blue</button>
+    </div>
+  );
+};
+
+export default Color;
